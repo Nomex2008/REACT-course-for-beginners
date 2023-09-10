bb902ff5175527c69f4df9f3ea1b9c3a1d1cd21e
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-09-10 22:08:21 +0300

    Lesіon 1
    Add lesson 1.

---
+++ lesson-1/code/js/mainJS.js
@@ -0,0 +1,69 @@
+const rootElement = document.getElementById('root');
+
+console.log(rootElement);
+
+//Main style
+const wrapperStyle = {
+    color: 'white',
+    background:'#000',
+    textAlign: 'center',
+    padding: '20px 0 0 0',
+    height:'100vh',
+}
+
+//style for objects
+//title
+const Title = {
+    fontFamily: 'Nunito',
+    fontSize: '50px',
+    fontStyle: 'normal',
+    fontWeight: '800',
+    lineHeight: '120%',  //60px 
+}
+//text
+const text = {
+    fontFamily: 'Nunito',
+    fontSize: '32px',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: '120%', // 48px //
+
+    padding:'40px 0 0 0',
+
+    maxWidth: '1000px',
+    margin: '0 auto',
+}
+const btn = {
+    color:'#fff',
+    background: 'blue',
+
+    padding:'10px 20px',
+    borderRadius: '8px',
+    display: 'inline-block',
+
+    margin: '20px auto 0',
+}
+
+//console style
+console.log(wrapperStyle,Title,text);
+
+//react DOM
+ReactDOM.createRoot(rootElement).render(
+
+    <div style={wrapperStyle}>
+
+        <h1 style={Title}>My projectin in React</h1>
+
+        <div style={text}>
+            <p>
+                REACT course for beginners. What is React, how does React work, where to use it? We are writing the first program!
+            </p>
+        </div>
+
+        <a className="btns" style={btn} href="https://github.com/Nomex2008">
+            My GitHub)
+        </a>
+
+    </div>
+
+);
